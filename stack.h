789abb2ecf147ec245037e01323b56e62e8a2133
vikramxsingh12 <7@felix-au.me>
2026-08-01 22:37:46 +0530

    Update stack.h - Step 7

--- stack.h
+++ stack.h
@@ -3,3 +3,9 @@ class Stack {
     std::vector<int> v;
 public:
     void push(int x) { v.push_back(x); }
+    int pop() {
+        int x = v.back();
+        v.pop_back();
+        return x;
+    }
+};
